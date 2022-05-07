7813206347349fbf99b3dd99cba78026fad38d94
Kai Yen <kai@purplesquirrel.io>
2022-05-07 17:53:37 -0400

    first version

---
+++ README.md
@@ -0,0 +1,18 @@
+# Airtable Next Auth Adapter
+
+This is experimental. Use at your own risk.
+
+This is a [next-auth](https://next-auth.js.org/) database adapter for [Airtable](https://airtable.com)
+
+## Airtable schema
+
+Clone this base in Airtable: https://airtable.com/shr16Xd8glUk90c4P
+
+## ENV vars
+
+Add your apiKey and the baseId of the cloned base to .env:
+
+```
+AIRTABLE_API_KEY=keyXXXXXXXXXXXXXX
+AIRTABLE_BASE_ID=appXXXXXXXXXXXXXX
+```
---
+++ src/airtableAdapter.ts
@@ -0,0 +1,219 @@
+import {
+  Adapter,
+  AdapterSession,
+  AdapterUser,
+  VerificationToken,
+} from 'next-auth/adapters'
+import Airtable, { FieldSet } from 'airtable'
+
+interface AirtableOptions {
+  apiKey: string
+  baseId: string
+}
+
+export default function AirtableAdapter(options: AirtableOptions): Adapter {
+  const { apiKey, baseId } = options
+  Airtable.configure({ apiKey })
+
+  const airtable = new Airtable()
+  const base = airtable.base(baseId)
+  const userTable = base.table('User')
+  const accountTable = base.table('Account')
+  const sessionTable = base.table('Session')
+  const verificationTable = base.table('VerificationToken')
+
+  async function getUserById(userId: string) {
+    console.log('getUserById')
+    return <Promise<AdapterUser>>(
+      userTable.find(userId).then((Record) => Record.fields)
+    )
+  }
+
+  async function getSessionBySessionToken(
+    sessionToken: string
+  ): Promise<AdapterSession> {
+    console.log('getSessionBySessionToken')
+    return <Promise<AdapterSession>>sessionTable
+      .select({ filterByFormula: `{sessionToken} = '${sessionToken}'` })
+      .all()
+      .then((Records) => (Records.length ? Records[0].fields : null))
+      .then((fields) => {
+        if (!fields) return null
+        const userId = Array.isArray(fields.userId)
+          ? fields.userId[0]
+          : fields.userId
+        return { ...fields, userId }
+      })
+  }
+
+  async function getAccountByProvider({
+    providerAccountId,
+    provider,
+  }: {
+    providerAccountId: string
+    provider: string
+  }) {
+    console.log('getAccountByProvider')
+    return accountTable
+      .select({
+        filterByFormula: `AND({providerAccountId}='${providerAccountId}', {provider}='${provider}')`,
+      })
+      .all()
+      .then((Records) => (Records.length > 0 ? Records[0].fields : {}))
+  }
+
+  async function getVerificationTokenByIdentifierAndToken({
+    identifier,
+    token,
+  }: {
+    identifier: string
+    token: string
+  }) {
+    console.log('getVerificationTokenbyIdentifierAndToken', identifier, token)
+    return <Promise<VerificationToken & { id?: string }>>verificationTable
+      .select({
+        filterByFormula: `AND({token}='${token}', {identifier}='${identifier}')`,
+      })
+      .all()
+      .then((Records) => (Records.length ? Records[0].fields : null))
+  }
+
+  return {
+    async createUser(user: any) {
+      return <Promise<AdapterUser>>(
+        userTable
+          .create([{ fields: user }])
+          .then((Records) => Records[0].fields)
+      )
+    },
+
+    async getUser(id) {
+      console.log('getUser', id)
+      return getUserById(id)
+    },
+
+    async getUserByEmail(email) {
+      console.log('getUserByEmail', email)
+      return <Promise<AdapterUser>>userTable
+        .select({ filterByFormula: `{email}='${email}'` })
+        .all()
+        .then((Records) => Records[0].fields)
+    },
+
+    async getUserByAccount({ providerAccountId, provider }) {
+      console.log('getUserByAccount', providerAccountId, provider)
+      const { userId } = await getAccountByProvider({
+        providerAccountId,
+        provider,
+      })
+
+      if (!userId) return null
+
+      return <Promise<AdapterUser>>(
+        userTable.find(userId.toString()).then((record) => record.fields)
+      )
+    },
+
+    async updateUser(user) {
+      console.log('updateUser', user)
+      const { id, ...userFields } = user
+      if (!id)
+        throw Error('Cannot update user. User id does not exist in user table')
+      await userTable.update(id, <Partial<FieldSet>>userFields)
+      return getUserById(id)
+    },
+
+    async deleteUser(userId) {
+      console.log('deleteUser', userId)
+      await userTable.destroy(userId)
+    },
+
+    async linkAccount(account) {
+      console.log('linkAccount', account)
+      const accountFields = { ...account, userId: [account.userId] }
+      await accountTable.create(accountFields)
+    },
+
+    async unlinkAccount({ providerAccountId, provider }) {
+      console.log('unlinkAccount', providerAccountId, provider)
+      const account = await getAccountByProvider({
+        providerAccountId,
+        provider,
+      })
+      const { id } = account
+      if (!id) throw Error('Could not unlink account.')
+      accountTable.destroy(id.toString())
+    },
+
+    async createSession({ sessionToken, userId, expires }) {
+      console.log('createSession')
+      const sessionFields = {
+        sessionToken,
+        expires: expires.toISOString(),
+        userId: [userId],
+      }
+      return <Promise<AdapterSession>>sessionTable
+        .create(sessionFields)
+        .then((Record) => Record.fields)
+        .then((fields) => ({ ...fields, expires: new Date(expires) }))
+    },
+
+    async getSessionAndUser(sessionToken) {
+      console.log('getSessionAndUser', sessionToken)
+      const session = await getSessionBySessionToken(sessionToken)
+      if (!session) return null
+
+      const user = await getUserById(session.userId)
+
+      return {
+        session: {
+          ...session,
+          expires: new Date(session.expires),
+          userId: session.userId[0],
+        },
+        user: { ...user, Account: undefined, Session: undefined },
+      }
+    },
+
+    async updateSession(newSession) {
+      console.log('updateSession', newSession)
+      const { sessionToken } = newSession
+      const { id } = await getSessionBySessionToken(sessionToken)
+      if (!id) return
+      await sessionTable.update(id, {
+        ...newSession,
+        expires: newSession.expires?.toISOString(),
+      })
+      return <Promise<AdapterSession>>(
+        sessionTable.find(id).then((Record) => Record.fields)
+      )
+    },
+
+    async deleteSession(sessionToken) {
+      console.log('deleteSession', sessionToken)
+      const sessionId = (await getSessionBySessionToken(sessionToken))?.id
+      if (!sessionId) return null
+      await sessionTable.destroy(sessionId)
+    },
+
+    async createVerificationToken(data) {
+      console.log('createVerificationToken', data)
+      return <Promise<VerificationToken>>(
+        verificationTable
+          .create({ ...data, expires: data.expires.toISOString() })
+          .then((Records) => Records.fields)
+      )
+    },
+
+    async useVerificationToken({ identifier, token }) {
+      console.log('useVerificationToken', identifier, token)
+      const verifier = await getVerificationTokenByIdentifierAndToken({
+        identifier,
+        token,
+      })
+      if (!verifier?.id) return null
+      verificationTable.destroy(verifier.id.toString())
+      return verifier
+    },
+  }
+}
